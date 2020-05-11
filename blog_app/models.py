0e31cd4e24d8201c44e5fc37cf3bf4454eb33c10
GiovMat <giovmat@gmail.com>
2020-05-11 11:50:16 +0200

    aggiunta articoli e categorie

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    cat_name = models.CharField(max_length=50, verbose_name='Category')
+    
+    def __str__(self):
+        return self.cat_name
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='article_pics', blank=True)
     pub_date = models.DateTimeField('date published')
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
