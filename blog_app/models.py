69fa417fd1a479aec2c4b7b6fd03e6b61cd0f215
Pasqat <pasquale.matarrese@gmail.com>
2020-05-12 14:59:08 +0200

    ...

--- blog_app/models.py
+++ blog_app/models.py
@@ -2,17 +2,25 @@ from django.db import models
 
 class Category(models.Model):
     cat_name = models.CharField(max_length=50, verbose_name='Category')
+    image = models.ImageField(upload_to='category_pics', blank=True)
     
     def __str__(self):
         return self.cat_name
 
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=50, verbose_name='Tag')
 
+    def __str__(self):
+        return self.tag_name
+    
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to='article_pics', blank=True)
     pub_date = models.DateTimeField('date published')
-    category = models.ManyToManyField(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
+    tag = models.ManyToManyField(Tag)
+    pin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -20,3 +28,4 @@ class Article(models.Model):
 ## TODO immagine in categoria
 ## TODO commenti agli articoli
 ## TODO pin articoli da tenere in homepgae
+## TODO Ricerche articoli per data o parloa chiave
