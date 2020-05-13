5ea2559e586573ed28e42c62212052b7524eca00
GiovMat <giovmat@gmail.com>
2020-05-13 08:09:32 +0200

    .

--- blog_prj/urls.py
+++ blog_prj/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog_app.urls')),
     path('', views.index, name ='index'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # ! questo sistema non va bene in deployment
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #FIXME ! questo sistema non va bene in deployment
